f006c488700826dca7b2f76a5f1da54d2e837724
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-02-19 21:32:15 +0900

    Set time zone and default locale

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,10 @@ module Baukis2
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.time_zone = "Tokyo"
+    config.i18n.load_path +=
+      Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
+    config.i18n.default_locale = :ja
   end
 end
