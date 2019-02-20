b68527c75174b4da7ac1ea6145dde3a822cf91d7
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-02-21 01:00:01 +0900

    Simplify exceptions_app

--- config/initializers/exceptions_app.rb
+++ config/initializers/exceptions_app.rb
@@ -1,12 +1,11 @@
 Rails.application.config.exceptions_app = lambda do |env|
   request = ActionDispatch::Request.new(env)
-  status = request.path_info[1..-1].to_i
 
   action =
-    case status
-      when 404; :not_found
-      when 422; :unprocessable_entity
-      else; :internal_server_error
+    case request.path_info
+    when "/404"; :not_found
+    when "/422"; :unprocessable_entity
+    else; :internal_server_error
     end
 
   ErrorsController.action(action).call(env)
