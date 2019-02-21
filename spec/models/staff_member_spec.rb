5846dce7481ea05a6641b433051c36cfb75ce449
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-02-21 13:00:21 +0000

    Impl. StaffMember#password=

---
+++ spec/models/staff_member_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe StaffMember, type: :model do
+  describe "#password=" do
+    example "文字列を与えると、hashed_passwordは長さ60の文字列になる" do
+      member = StaffMember.new
+      member.password = "baukis"
+      expect(member.hashed_password).to be_kind_of(String)
+      expect(member.hashed_password.size).to eq(60)
+    end
+
+    example "nilを与えると、hashed_passwordはnilになる" do
+      member = StaffMember.new(hashed_password: "x")
+      member.password = nil
+      expect(member.hashed_password).to be_nil
+    end
+  end
+end
