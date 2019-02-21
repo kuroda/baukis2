56037edadac9b313106273357b2d795283032a1e
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-02-21 13:18:15 +0000

    Add routes for staff/session

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :staff do
     root "top#index"
+    get "login" => "sessions#new", as: :login
+    post "session" => "sessions#create", as: :session
+    delete "session" => "sessions#destroy"
   end
 
   namespace :admin do
