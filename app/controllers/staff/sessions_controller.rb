dd299b50e1f4a52988490b68d6f73a3b6a25a27b
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-02-21 13:49:15 +0000

    Create staff/sessions#create

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -7,4 +7,20 @@ class Staff::SessionsController < Staff::Base
       render action: "new"
     end
   end
+
+  def create
+    @form = Staff::LoginForm.new(params[:staff_login_form])
+
+    if @form.email.present?
+      staff_member = StaffMember.find_by(email_for_index: @form.email.downcase)
+    end
+
+    if staff_member
+      session[:staff_member_id] = staff_member.id
+      flash.notice = "ログインしました。"
+      redirect_to :staff_root
+    else
+      render action: "new"
+    end
+  end
 end
