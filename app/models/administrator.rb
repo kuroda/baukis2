a1531efda15a8564e39f2ed5b7dff0a14c74c0fd
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-02-23 10:22:37 +0900

    [backport] Add Administrator model and its seed data

---
+++ app/models/administrator.rb
@@ -0,0 +1,13 @@
+class Administrator < ApplicationRecord
+  before_validation do
+    self.email_for_index = email.downcase if email
+  end
+
+  def password=(raw_password)
+    if raw_password.kind_of?(String)
+      self.hashed_password = BCrypt::Password.create(raw_password)
+    elsif raw_password.nil?
+      self.hashed_password = nil
+    end
+  end
+end
