cbec28030b0b8ad010a9bb05cce3f75fdad1391e
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-02-23 22:05:47 +0900

    Change routes: session, account

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,15 @@ Rails.application.routes.draw do
   namespace :staff, path: "" do
     root "top#index"
     get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destroy"
-    resources :staff_members
+    resource :session, only: [ :create, :destroy ]
+    resource :account, only: [ :new, :create, :destroy ]
   end
 
   namespace :admin do
     root "top#index"
     get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destroy"
+    resource :session, only: [ :create, :destroy ]
+    resources :staff_members
   end
 
   namespace :customer do
