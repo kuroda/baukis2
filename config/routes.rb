973ab089fc53d34b980045d2f1e8d52799565d32
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-02-23 10:30:51 +0900

    [backport] Put login/logout links on admin header

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root "top#index"
+    get "login" => "sessions#new", as: :login
+    post "session" => "sessions#create", as: :session
+    delete "session" => "sessions#destroy"
   end
 
   namespace :customer do
