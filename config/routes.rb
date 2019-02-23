1ba816dc96f5f16f9c5df04d36c4bca47d6556c1
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-02-23 22:16:40 +0900

    Add routing constraints to staff/admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
 Rails.application.routes.draw do
-  namespace :staff, path: "" do
-    root "top#index"
-    get "login" => "sessions#new", as: :login
-    resource :session, only: [ :create, :destroy ]
-    resource :account, only: [ :new, :create, :destroy ]
+  config = Rails.application.config.baukis2
+
+  constraints host: config[:staff][:host] do
+    namespace :staff, path: config[:staff][:path] do
+      root "top#index"
+      get "login" => "sessions#new", as: :login
+      resource :session, only: [ :create, :destroy ]
+      resource :account, only: [ :new, :create, :destroy ]
+    end
   end
 
-  namespace :admin do
-    root "top#index"
-    get "login" => "sessions#new", as: :login
-    resource :session, only: [ :create, :destroy ]
-    resources :staff_members
+  constraints host: config[:admin][:host] do
+    namespace :admin, path: config[:admin][:path] do
+      root "top#index"
+      get "login" => "sessions#new", as: :login
+      resource :session, only: [ :create, :destroy ]
+      resources :staff_members
+    end
   end
 
   namespace :customer do
