614a9390c1922a15072559b95bd3d2a4a470aacb
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-02-26 00:08:14 +0900

    Adapt admin/staff_members to Strong Parameters

--- app/controllers/admin/staff_members_controller.rb
+++ app/controllers/admin/staff_members_controller.rb
@@ -17,7 +17,7 @@ class Admin::StaffMembersController < Admin::Base
   end
 
   def create
-    @staff_member = StaffMember.new(params[:staff_member])
+    @staff_member = StaffMember.new(staff_member_params)
     if @staff_member.save
       flash.notice = "職員アカウントを新規登録しました。"
       redirect_to :admin_staff_members
@@ -28,7 +28,7 @@ class Admin::StaffMembersController < Admin::Base
 
   def update
     @staff_member = StaffMember.find(params[:id])
-    @staff_member.assign_attributes(params[:staff_member])
+    @staff_member.assign_attributes(staff_member_params)
     if @staff_member.save
       flash.notice = "職員アカウントを更新しました。"
       redirect_to :admin_staff_members
@@ -42,5 +42,13 @@ class Admin::StaffMembersController < Admin::Base
     staff_member.destroy!
     flash.notice = "職員アカウントを削除しました。"
     redirect_to :admin_staff_members
+  end
+
+  private def staff_member_params
+    params.require(:staff_member).permit(
+      :email, :password, :family_name, :given_name,
+      :family_name_kana, :given_name_kana,
+      :start_date, :end_date, :suspended
+    )
   end
 end
