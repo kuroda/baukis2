ad9118dd5337df643a95bec472f70fd6b31da433
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-04-04 00:03:24 +0900

    [backport] Fold a long line

--- app/controllers/admin/staff_members_controller.rb
+++ app/controllers/admin/staff_members_controller.rb
@@ -1,6 +1,7 @@
 class Admin::StaffMembersController < Admin::Base
   def index
-    @staff_members = StaffMember.order(:family_name_kana, :given_name_kana)
+    @staff_members = StaffMember
+      .order(:family_name_kana, :given_name_kana)
   end
 
   def show
