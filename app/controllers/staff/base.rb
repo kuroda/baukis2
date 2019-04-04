b7cd8b662af134e1f319111fdfbbf8427c8d8661
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-04-04 22:35:52 +0900

    [CH12] Add check_timeout before_action (p.269-271)

--- app/controllers/staff/base.rb
+++ app/controllers/staff/base.rb
@@ -1,6 +1,7 @@
 class Staff::Base < ApplicationController
   before_action :authorize
   before_action :check_account
+  before_action :check_timeout
 
   private def current_staff_member
     if session[:staff_member_id]
@@ -23,6 +24,20 @@ class Staff::Base < ApplicationController
       session.delete(:staff_member_id)
       flash.alert = "アカウントが無効になりました。"
       redirect_to :staff_root
+    end
+  end
+
+  TIMEOUT = 60.minutes
+
+  private def check_timeout
+    if current_staff_member
+      if session[:last_access_time] >= TIMEOUT.ago
+        session[:last_access_time] = Time.current
+      else
+        session.delete(:staff_member_id)
+        flash.alert = "セッションがタイムアウトしました。"
+        redirect_to :staff_root
+      end
     end
   end
 end
