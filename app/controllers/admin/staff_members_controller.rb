f291e9465c82d50917d919c85406b97e84e769c0
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-04-04 13:05:10 +0900

    Revert "[backport] Fold a long line"
    This reverts commit ad9118dd5337df643a95bec472f70fd6b31da433.

--- app/controllers/admin/staff_members_controller.rb
+++ app/controllers/admin/staff_members_controller.rb
@@ -1,7 +1,6 @@
 class Admin::StaffMembersController < Admin::Base
   def index
-    @staff_members = StaffMember
-      .order(:family_name_kana, :given_name_kana)
+    @staff_members = StaffMember.order(:family_name_kana, :given_name_kana)
   end
 
   def show
