a9bfa060699f2c4a48ecf191e68a80dcf18832ff
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-04-05 23:38:42 +0900

    [CH12] Minor fix to admin/staff_members_management_spec

--- spec/requests/admin/staff_members_management_spec.rb
+++ spec/requests/admin/staff_members_management_spec.rb
@@ -5,7 +5,6 @@ describe "管理者による職員管理", "ログイン前" do
 end
 
 describe "管理者による職員管理" do
-  let(:params_hash) { attributes_for(:staff_member) }
   let(:administrator) { create(:administrator) }
 
   describe "一覧" do
@@ -27,6 +26,8 @@ describe "管理者による職員管理" do
   end
 
   describe "新規登録" do
+    let(:params_hash) { attributes_for(:staff_member) }
+
     before do
       login_as(administrator)
     end
@@ -44,6 +45,7 @@ describe "管理者による職員管理" do
 
   describe "更新" do
     let(:staff_member) { create(:staff_member) }
+    let(:params_hash) { attributes_for(:staff_member) }
 
     before do
       login_as(administrator)
