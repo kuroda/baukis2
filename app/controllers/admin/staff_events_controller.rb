4e0d18ae8521b06fd6dc5da4b075f52701509e66
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-04-06 21:19:35 +0900

    [CH13] section-2: staff_events#index

---
+++ app/controllers/admin/staff_events_controller.rb
@@ -0,0 +1,10 @@
+class Admin::StaffEventsController < Admin::Base
+  def index
+    if params[:staff_member_id]
+      @staff_member = StaffMember.find(params[:staff_member_id])
+      @events = @staff_member.events.order(occurred_at: :desc)
+    else
+      @events = StaffEvent.order(occurred_at: :desc)
+    end
+  end
+end
