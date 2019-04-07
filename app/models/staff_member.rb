afb80c2c2135f4acb9433daa85341043aebd005a
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-04-07 11:30:16 +0900

    [CH14] normalize and validate emails

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -4,6 +4,7 @@ class StaffMember < ApplicationRecord
   has_many :events, class_name: "StaffEvent", dependent: :destroy
 
   before_validation do
+    self.email = normalize_as_email(email)
     self.email_for_index = email.downcase if email
     self.family_name = normalize_as_name(family_name)
     self.given_name = normalize_as_name(given_name)
@@ -13,6 +14,7 @@ class StaffMember < ApplicationRecord
 
   KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/
 
+  validates :email, presence: true, email: { allow_blank: true }
   validates :family_name, :given_name, presence: true
   validates :family_name_kana, :given_name_kana, presence: true,
     format: { with: KATAKANA_REGEXP, allow_blank: true }
@@ -26,6 +28,14 @@ class StaffMember < ApplicationRecord
     before: -> (obj) { 1.year.from_now.to_date },
     allow_blank: true
   }
+
+  validates :email_for_index, uniqueness: { allow_blank: true }
+  after_validation do
+    if errors.include?(:email_for_index)
+      errors.add(:email, :taken)
+      errors.delete(:email_for_index)
+    end
+  end
 
   def password=(raw_password)
     if raw_password.kind_of?(String)
