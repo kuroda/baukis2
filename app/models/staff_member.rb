dccf035bbcda811c7c0239b5958c36e33d29eb6e
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-04-07 10:48:47 +0900

    [CH14] normalize names

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,8 +1,14 @@
 class StaffMember < ApplicationRecord
+  include StringNormalizer
+
   has_many :events, class_name: "StaffEvent", dependent: :destroy
 
   before_validation do
     self.email_for_index = email.downcase if email
+    self.family_name = normalize_as_name(family_name)
+    self.given_name = normalize_as_name(given_name)
+    self.family_name_kana = normalize_as_furigana(family_name_kana)
+    self.given_name_kana = normalize_as_furigana(given_name_kana)
   end
 
   KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/
