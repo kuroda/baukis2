dce942be8ff4ece1879be00f426370fea91ebb5f
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-08-01 09:41:25 +0900

    [CH16] Add link to :staff_customers

--- app/controllers/staff/top_controller.rb
+++ app/controllers/staff/top_controller.rb
@@ -2,5 +2,10 @@ class Staff::TopController < Staff::Base
   skip_before_action :authorize
 
   def index
+    if current_staff_member
+      render action: "dashboard"
+    else
+      render action: "index"
+    end
   end
 end
