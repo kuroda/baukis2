556776f5349c7fe029c677420a2539bbfc5fa603
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-09-27 23:00:46 +0900

    sec-4-1: rspec basics

---
+++ spec/experiments/string_spec.rb
@@ -0,0 +1,16 @@
+require "spec_helper"
+
+describe String do
+  describe "#<<" do
+    example "文字の追加" do
+      s = "ABC"
+      s << "D"
+      expect(s.size).to eq(4)
+    end
+
+    example "nilは追加できない", :exception do
+      s = "ABC"
+      expect { s << nil }.to raise_error(TypeError)
+    end
+  end
+end
