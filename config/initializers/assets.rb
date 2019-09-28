017bda7939db122606777d141abd1749082bacbb
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-09-28 16:20:31 +0900

    sec5-4 (wip)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,3 @@
 Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.paths << Rails.root.join("node_modules")
-Rails.application.config.assets.precompile += %w( staff.css )
+Rails.application.config.assets.precompile += %w( staff.css admin.css customer.css )
