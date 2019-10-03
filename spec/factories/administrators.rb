e67d002b71152905d0c8fd5fd49b5670717a1bf8
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-10-03 22:07:42 +0900

    sec8-4 (d)

--- spec/factories/administrators.rb
+++ spec/factories/administrators.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :administrator do
     sequence(:email) { |n| "admin#{n}@example.com" }
-    password "pw"
-    suspended false
+    password { "pw" }
+    suspended { false }
   end
 end
