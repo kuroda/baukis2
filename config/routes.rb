301f608f8c20b205d88f09ea114c5adf8d4bbf90
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-10-04 23:22:22 +0900

    sec9-1, sec9-2

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
     get "login" => "sessions#new", as: :login
     post "session" => "sessions#create", as: :session
     delete "session" => "sessions#destroy"
+    resources :staff_members
   end
 
   namespace :customer do
