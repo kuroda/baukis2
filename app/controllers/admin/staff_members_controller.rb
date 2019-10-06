0c9b92c7391c431ca63cf78d9afa0c28871d6d1b
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-10-06 09:33:07 +0900

    sec10-2

--- app/controllers/admin/staff_members_controller.rb
+++ app/controllers/admin/staff_members_controller.rb
@@ -15,4 +15,32 @@ class Admin::StaffMembersController < Admin::Base
   def edit
     @staff_member = StaffMember.find(params[:id])
   end
+
+  def create
+    @staff_member = StaffMember.new(params[:staff_member])
+    if @staff_member.save
+      flash.notice = "職員アカウントを新規登録しました。"
+      redirect_to :admin_staff_members
+    else
+      render action: "new"
+    end
+  end
+
+  def update
+    @staff_member = StaffMember.find(params[:id])
+    @staff_member.assign_attributes(params[:staff_member])
+    if @staff_member.save
+      flash.notice = "職員アカウントを更新しました。"
+      redirect_to :admin_staff_members
+    else
+      render action: "edit"
+    end
+  end
+
+  def destroy
+    staff_member = StaffMember.find(params[:id])
+    staff_member.destroy!
+    flash.notice = "職員アカウントを削除しました。"
+    redirect_to :admin_staff_members
+  end
 end
