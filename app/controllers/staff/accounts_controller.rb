94fe649d6a19d8a6ded6bb334dcc4f5234d80492
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-10-06 15:18:43 +0900

    sec11-2

---
+++ app/controllers/staff/accounts_controller.rb
@@ -0,0 +1,27 @@
+class Staff::AccountsController < Staff::Base
+  def show
+    @staff_member = current_staff_member
+  end
+
+  def edit
+    @staff_member = current_staff_member
+  end
+
+  def update
+    @staff_member = current_staff_member
+    @staff_member.assign_attributes(staff_member_params)
+    if @staff_member.save
+      flash.notice = "アカウント情報を更新しました。"
+      redirect_to :staff_account
+    else
+      render action: "edit"
+    end
+  end
+
+  private def staff_member_params
+    params.require(:staff_member).permit(
+      :email, :family_name, :given_name,
+      :family_name_kana, :given_name_kana
+    )
+  end
+end
--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -9,7 +9,7 @@ class Staff::SessionsController < Staff::Base
   end
 
   def create
-    @form = Staff::LoginForm.new(params[:staff_login_form])
+    @form = Staff::LoginForm.new(login_form_params)
     if @form.email.present?
       staff_member =
         StaffMember.find_by("LOWER(email) = ?", @form.email.downcase)
@@ -27,6 +27,10 @@ class Staff::SessionsController < Staff::Base
       flash.now.alert = "メールアドレスまたはパスワードが正しくありません。"
       render action: "new"
     end
+  end
+
+  private def login_form_params
+    params.require(:staff_login_form).permit(:email, :password)
   end
 
   def destroy
