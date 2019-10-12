d7a4cd5df2eacbd72d8e3e25ecab8b1c7a26eaa5
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-10-12 16:04:12 +0900

    sec13-3 (a)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-table_names = %w(staff_members administrators)
+table_names = %w(staff_members administrators staff_events)
 
 table_names.each do |table_name|
   path = Rails.root.join("db", "seeds", Rails.env, "#{table_name}.rb")
