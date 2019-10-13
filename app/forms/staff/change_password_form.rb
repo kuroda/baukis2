cbe786fed5d4b8d97100be3c1f5d1d21faf2b052
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-10-13 11:40:24 +0900

    sec14-2 (c)

--- app/forms/staff/change_password_form.rb
+++ app/forms/staff/change_password_form.rb
@@ -1,11 +1,20 @@
-class Staff::ChangePasswordForm 
+class Staff::ChangePasswordForm
   include ActiveModel::Model
 
   attr_accessor :object, :current_password, :new_password,
     :new_password_confirmation
+  validates :new_password, presence: true, confirmation: true
+
+  validate do
+    unless Staff::Authenticator.new(object).authenticate(current_password)
+      errors.add(:current_password, :wrong)
+    end
+  end
 
   def save
-    object.password = new_password
-    object.save!
+    if valid?
+      object.password = new_password
+      object.save!
+    end
   end
 end
