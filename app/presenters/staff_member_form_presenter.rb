f3c33b813f2412c221d3588f73ba38cfd62d4448
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-10-18 13:57:09 +0900

    [ch15] fix staff_member_form_presenter.rb

--- app/presenters/staff_member_form_presenter.rb
+++ app/presenters/staff_member_form_presenter.rb
@@ -16,7 +16,7 @@ class StaffMemberFormPresenter < FormPresenter
   end
 
   def suspended_check_box
-    markup(:div, class: "input-block") do |m|
+    markup(:div, class: "check-boxes") do |m|
       m << check_box(:suspended)
       m << label(:suspended, "アカウント停止")
     end
