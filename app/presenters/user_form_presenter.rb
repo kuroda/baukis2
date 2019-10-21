254238da7a0a82e20abee1562233fdbf8689ea24
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-10-21 11:26:29 +0900

    sec16-3

---
+++ app/presenters/user_form_presenter.rb
@@ -0,0 +1,17 @@
+class UserFormPresenter < FormPresenter
+  def password_field_block(name, label_text, options = {})
+    if object.new_record?
+      super(name, label_text, options)
+    end
+  end
+
+  def full_name_block(name1, name2, label_text, options = {})
+    markup(:div, class: "input-block") do |m|
+      m << decorated_label(name1, label_text, options)
+      m << text_field(name1, options)
+      m << text_field(name2, options)
+      m << error_messages_for(name1)
+      m << error_messages_for(name2)
+    end
+  end
+end
