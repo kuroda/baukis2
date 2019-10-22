9277b7b233f1f58bcc5a8cf71b25884e6a44eb1d
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-10-22 19:39:47 +0900

    sec18-3 (b)

--- app/forms/staff/customer_form.rb
+++ app/forms/staff/customer_form.rb
@@ -14,6 +14,9 @@ class Staff::CustomerForm
     self.inputs_work_address = @customer.work_address.present?
     @customer.build_home_address unless @customer.home_address
     @customer.build_work_address unless @customer.work_address
+    (2 - @customer.home_address.phones.size).times do
+      @customer.home_address.phones.build
+    end
   end
 
   def assign_attributes(params = {})
@@ -35,6 +38,16 @@ class Staff::CustomerForm
 
     if inputs_home_address
       customer.home_address.assign_attributes(home_address_params)
+      phones = phone_params(:home_address).fetch(:phones)
+
+      customer.home_address.phones.size.times do |index|
+        attributes = phones[index.to_s]
+        if attributes && attributes[:number].present?
+          customer.home_address.phones[index].assign_attributes(attributes)
+        else
+          customer.home_address.phones[index].mark_for_destruction
+        end
+      end
     else
       customer.home_address.mark_for_destruction
     end
@@ -55,7 +68,7 @@ class Staff::CustomerForm
   end
 
   private def home_address_params
-    @params.require(:home_address).permit(
+    @params.require(:home_address).except(:phones).permit(
       :postal_code, :prefecture, :city, :address1, :address2,
     )
   end
