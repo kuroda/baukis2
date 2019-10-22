a254b39bf0b2e5380af85ea1a72b64c8a035a966
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-10-22 11:50:45 +0900

    sec18-1 (a)

--- app/forms/staff/customer_form.rb
+++ app/forms/staff/customer_form.rb
@@ -1,12 +1,14 @@
 class Staff::CustomerForm
   include ActiveModel::Model
 
-  attr_accessor :customer
+  attr_accessor :customer, :inputs_home_address, :inputs_work_address
   delegate :persisted?, :save, to: :customer
 
   def initialize(customer = nil)
     @customer = customer
     @customer ||= Customer.new(gender: "male")
+    self.inputs_home_address = @customer.home_address.present?
+    self.inputs_work_address = @customer.work_address.present?
     @customer.build_home_address unless @customer.home_address
     @customer.build_work_address unless @customer.work_address
   end
