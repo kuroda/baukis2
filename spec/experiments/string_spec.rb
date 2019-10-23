4b3372758fb9e03e1c0cefe1b02bac93ac6a36d7
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2019-10-23 16:03:24 +0900

    Section 4-2

--- spec/experiments/string_spec.rb
+++ spec/experiments/string_spec.rb
@@ -7,5 +7,11 @@ describe String do
       s << "D"
       expect(s.size).to eq(4)
     end
+
+    xexample "nilの追加" do
+      s = "ABC"
+      s << nil
+      expect(s.size).to eq(4)
+    end
   end
 end
