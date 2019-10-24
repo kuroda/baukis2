f75c56a4a57bdcca467f60b0bb52fa11addcb24b
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2019-10-24 12:26:45 +0900

    Section 6-2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
 
+  rescue_from StandardError, with: :rescue500
+
   private def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
       Regexp.last_match[1]
     else
       "customer"
     end
+  end
+
+  private def rescue500(e)
+    render "errors/internal_server_error", status: 500
   end
 end
