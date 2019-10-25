ebfeda88d4770997237d75428791dd3cb8e0441e
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2019-10-25 14:32:22 +0900

    Section 9-4

---
+++ spec/routing/hostname_constrants_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+describe "ルーティング" do
+  let(:config) { Rails.application.config.baukis2 }
+
+  example "職員トップページ" do
+    url = "http://#{config[:staff][:host]}/#{config[:staff][:path]}"
+    expect(get: url).to route_to(
+      host: config[:staff][:host],
+      controller: "staff/top",
+      action: "index"
+    )
+  end
+
+  example "管理者ログインフォーム" do
+    url = "http://#{config[:admin][:host]}/#{config[:admin][:path]}/login"
+    expect(get: url).to route_to(
+      host: config[:admin][:host],
+      controller: "admin/sessions",
+      action: "new"
+    )
+  end
+
+  example "ホスト名が対象外ならroutableではない" do
+    expect(get: "http://foo.example.com").not_to be_routable
+  end
+
+  example "存在しないパスならroutableではない" do
+    expect(get: "http://#{config[:staff][:host]}/xyz").not_to be_routable
+  end
+end
