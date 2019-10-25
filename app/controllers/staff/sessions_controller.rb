34c973f89f55fe87cfe9472564fce0fe27621535
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2019-10-25 12:59:32 +0900

    Section 8-3

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -15,15 +15,23 @@ class Staff::SessionsController < Staff::Base
         StaffMember.find_by("LOWER(email) = ?", @form.email.downcase)
     end
     if Staff::Authenticator.new(staff_member).authenticate(@form.password)
-      session[:staff_member_id] = staff_member.id
-      redirect_to :staff_root
+      if staff_member.suspended?
+        flash.now.alert = "アカウントが停止されています。"
+        render action: "new"
+      else
+        session[:staff_member_id] = staff_member.id
+        flash.notice = "ログインしました。"
+        redirect_to :staff_root
+      end
     else
+      flash.now.alert = "メールアドレスまたはパスワードが正しくありません。"
       render action: "new"
     end
   end
 
   def destroy
     session.delete(:staff_member_id)
+    flash.notice = "ログアウトしました。"
     redirect_to :staff_root
   end
 end
--- app/services/staff/authenticator.rb
+++ app/services/staff/authenticator.rb
@@ -5,7 +5,6 @@ class Staff::Authenticator
 
   def authenticate(raw_password)
     @staff_member &&
-      !@staff_member.suspended? &&
       @staff_member.hashed_password &&
       @staff_member.start_date <= Date.today &&
       (@staff_member.end_date.nil? || @staff_member.end_date > Date.today) &&
