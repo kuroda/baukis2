ccdf214c1a35b656b77a045e61219842290b45ca
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2019-10-25 14:01:05 +0900

    Section 9-2

--- app/controllers/admin/top_controller.rb
+++ app/controllers/admin/top_controller.rb
@@ -1,5 +1,9 @@
 class Admin::TopController < Admin::Base
   def index
-    render action: "index"
+    if current_administrator
+      render action: "dashboard"
+    else
+      render action: "index"
+    end
   end
 end
