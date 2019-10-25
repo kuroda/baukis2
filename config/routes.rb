2729b587f909d57b0ac0af4017423c4e2b345fa8
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2019-10-25 14:05:35 +0900

    Section 9-3

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,14 @@ Rails.application.routes.draw do
   namespace :staff, path: "" do
     root "top#index"
     get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destroy"
+    resource :session, only: [ :create, :destroy ]
+    resource :account, except: [ :new, :create, :destroy ]
   end
 
   namespace :admin do
     root "top#index"
     get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destroy"
+    resource :session, only: [ :create, :destroy ]
     resources :staff_members
   end
 
