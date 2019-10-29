43c4ce8d10a9b934f89d848f6326acbaaad2062a
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2019-10-29 11:30:21 +0900

    Section 13-2

--- app/models/staff_event.rb
+++ app/models/staff_event.rb
@@ -3,4 +3,14 @@ class StaffEvent < ApplicationRecord
 
   belongs_to :member, class_name: "StaffMember", foreign_key: "staff_member_id"
   alias_attribute :occurred_at, :created_at
+
+  DESCRIPTIONS = {
+    logged_in: "ログイン",
+    logged_out: "ログアウト",
+    rejected: "ログイン拒否"
+  }
+
+  def description
+    DESCRIPTIONS[type.to_sym]
+  end
 end
