3d4c7f15c788ff471968e12979a2e53419956bcb
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2019-10-29 11:51:49 +0900

    Section 13-4

--- app/controllers/admin/staff_events_controller.rb
+++ app/controllers/admin/staff_events_controller.rb
@@ -2,10 +2,11 @@ class Admin::StaffEventsController < Admin::Base
   def index
     if params[:staff_member_id]
       @staff_member = StaffMember.find(params[:staff_member_id])
-      @events = @staff_member.events.order(occurred_at: :desc)
+      @events = @staff_member.events
     else
-      @events = StaffEvent.order(occurred_at: :desc)
+      @events = StaffEvent
     end
-    @events = @events.page(params[:page])
+    @events =
+      @events.order(occurred_at: :desc).includes(:member).page(params[:page])
   end
 end
