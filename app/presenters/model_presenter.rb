39ceb24ff92c0a683f304576a6214913a061445b
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2019-11-05 11:42:54 +0900

    Section 15-2

--- app/presenters/model_presenter.rb
+++ app/presenters/model_presenter.rb
@@ -1,6 +1,8 @@
 class ModelPresenter
+  include HtmlBuilder
+
   attr_reader :object, :view_context
-  delegate :raw, to: :view_context
+  delegate :raw, :link_to, to: :view_context
 
   def initialize(object, view_context)
     @object = object
