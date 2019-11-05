bea91db1148d3d2d2169021f65ce0f97a5d819a1
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2019-11-05 16:37:33 +0900

    Section 16-4

--- app/forms/staff/customer_form.rb
+++ app/forms/staff/customer_form.rb
@@ -10,4 +10,40 @@ class Staff::CustomerForm
     @customer.build_home_address unless @customer.home_address
     @customer.build_work_address unless @customer.work_address
   end
+
+  def assign_attributes(params = {})
+    @params = params
+    customer.assign_attributes(customer_params)
+    customer.home_address.assign_attributes(home_address_params)
+    customer.work_address.assign_attributes(work_address_params)
+  end
+
+  def save
+    ActiveRecord::Base.transaction do
+      customer.save!
+      customer.home_address.save!
+      customer.work_address.save!
+    end
+  end
+
+  private def customer_params
+    @params.require(:customer).permit(
+      :email, :password,
+      :family_name, :given_name, :family_name_kana, :given_name_kana,
+      :birthday, :gender
+    )
+  end
+
+  private def home_address_params
+    @params.require(:home_address).permit(
+      :postal_code, :prefecture, :city, :address1, :address2
+    )
+  end
+
+  private def work_address_params
+    @params.require(:work_address).permit(
+      :postal_code, :prefecture, :city, :address1, :address2,
+      :company_name, :division_name
+    )
+  end
 end
