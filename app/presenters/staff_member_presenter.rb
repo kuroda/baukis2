6ae6cbfeaf6a770923d0ae024c7e1c5f1c620c14
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2019-11-05 13:13:47 +0900

    Section 15-5

--- app/presenters/staff_member_presenter.rb
+++ app/presenters/staff_member_presenter.rb
@@ -1,6 +1,14 @@
 class StaffMemberPresenter < ModelPresenter
   delegate :suspended?, to: :object
 
+  def full_name
+    object.family_name + " " + object.given_name
+  end
+
+  def full_name_kana
+    object.family_name_kana + " " + object.given_name_kana
+  end
+
   # 職員の停止フラグのOn/Offを表現する記号を返す。
   # On: BALLOT BOX WITH CHECK (U+2611)
   # Off: BALLOT BOX (U+2610)
