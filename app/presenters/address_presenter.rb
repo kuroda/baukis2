b5d8441d57458bfbd65f08e61d878d7911c4f380
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2019-11-06 09:29:53 +0900

    Section 16-2

---
+++ app/presenters/address_presenter.rb
@@ -0,0 +1,12 @@
+class AddressPresenter < ModelPresenter
+  delegate :prefecture, :city, :address1, :address2,
+    :company_name, :division_name, to: :object
+
+  def postal_code
+    if md = object.postal_code.match(/\A(\d{3})(\d{4})\z/)
+      md[1] + "-" + md[2]
+    else
+      object.postal_code
+    end
+  end
+end
