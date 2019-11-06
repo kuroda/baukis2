5db1b0b47174606e52a55f72e044812f06d8ee8f
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2019-11-06 15:34:50 +0900

    Section 18-1

--- app/forms/staff/customer_form.rb
+++ app/forms/staff/customer_form.rb
@@ -1,22 +1,36 @@
 class Staff::CustomerForm
   include ActiveModel::Model
 
-  attr_accessor :customer
+  attr_accessor :customer, :inputs_home_address, :inputs_work_address
   delegate :persisted?, :save, to: :customer
 
   def initialize(customer = nil)
     @customer = customer
     @customer ||= Customer.new(gender: "male")
+    self.inputs_home_address = @customer.home_address.present?
+    self.inputs_work_address = @customer.work_address.present?
     @customer.build_home_address unless @customer.home_address
     @customer.build_work_address unless @customer.work_address
   end
 
   def assign_attributes(params = {})
     @params = params
+    self.inputs_home_address = params[:inputs_home_address] == "1"
+    self.inputs_work_address = params[:inputs_work_address] == "1"
 
     customer.assign_attributes(customer_params)
-    customer.home_address.assign_attributes(home_address_params)
-    customer.work_address.assign_attributes(work_address_params)
+
+    if inputs_home_address
+      customer.home_address.assign_attributes(home_address_params)
+    else
+      customer.home_address.mark_for_destruction
+    end
+
+    if inputs_work_address
+      customer.work_address.assign_attributes(work_address_params)
+    else
+      customer.work_address.mark_for_destruction
+    end
   end
 
   private def customer_params
