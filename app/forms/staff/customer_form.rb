53b51d2fa7106322160cddd725ea7904c68909bb
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2019-11-07 11:42:58 +0900

    Section 18-3

--- app/forms/staff/customer_form.rb
+++ app/forms/staff/customer_form.rb
@@ -8,9 +8,15 @@ class Staff::CustomerForm
     @customer = customer
     @customer ||= Customer.new(gender: "male")
     self.inputs_home_address = @customer.home_address.present?
+    (2 - @customer.personal_phones.size).times do
+      @customer.personal_phones.build
+    end
     self.inputs_work_address = @customer.work_address.present?
     @customer.build_home_address unless @customer.home_address
     @customer.build_work_address unless @customer.work_address
+    (2 - @customer.home_address.phones.size).times do
+      @customer.home_address.phones.build
+    end
   end
 
   def assign_attributes(params = {})
@@ -19,9 +25,29 @@ class Staff::CustomerForm
     self.inputs_work_address = params[:inputs_work_address] == "1"
 
     customer.assign_attributes(customer_params)
+    phones = phone_params(:customer).fetch(:phones)
+
+    customer.personal_phones.size.times do |index|
+      attributes = phones[index.to_s]
+      if attributes && attributes[:number].present?
+        customer.personal_phones[index].assign_attributes(attributes)
+      else
+        customer.personal_phones[index].mark_for_destruction
+      end
+    end
 
     if inputs_home_address
       customer.home_address.assign_attributes(home_address_params)
+      phones = phone_params(:home_address).fetch(:phones)
+
+      customer.home_address.phones.size.times do |index|
+        attributes = phones[index.to_s]
+        if attributes && attributes[:number].present?
+          customer.home_address.phones[index].assign_attributes(attributes)
+        else
+          customer.home_address.phones[index].mark_for_destruction
+        end
+      end
     else
       customer.home_address.mark_for_destruction
     end
@@ -34,7 +60,7 @@ class Staff::CustomerForm
   end
 
   private def customer_params
-    @params.require(:customer).permit(
+    @params.require(:customer).except(:phones).permit(
       :email, :password,
       :family_name, :given_name, :family_name_kana, :given_name_kana,
       :birthday, :gender
@@ -42,7 +68,7 @@ class Staff::CustomerForm
   end
 
   private def home_address_params
-    @params.require(:home_address).permit(
+    @params.require(:home_address).except(:phones).permit(
       :postal_code, :prefecture, :city, :address1, :address2
     )
   end
@@ -52,5 +78,10 @@ class Staff::CustomerForm
       :postal_code, :prefecture, :city, :address1, :address2,
       :company_name, :division_name
     )
+  end
+
+  private def phone_params(record_name)
+    @params.require(record_name)
+      .slice(:phones).permit(phones: [ :number, :primary ])
   end
 end
