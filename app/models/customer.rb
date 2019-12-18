443849351f7218eccd3be3b65fef85011f3bf919
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-12-18 10:16:14 +0900

    [book2] Section 02-2 (b)

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,8 +3,9 @@ class Customer < ApplicationRecord
   include PersonalNameHolder
   include PasswordHolder
 
-  has_one :home_address, dependent: :destroy, autosave: true
-  has_one :work_address, dependent: :destroy, autosave: true
+  has_many :addresses, dependent: :destroy
+  has_one :home_address, autosave: true
+  has_one :work_address, autosave: true
   has_many :phones, dependent: :destroy
   has_many :personal_phones, -> { where(address_id: nil).order(:id) },
     class_name: "Phone", autosave: true
