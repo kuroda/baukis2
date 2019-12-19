6b091f9dd14bef4a3d90314083ae0d1cb891d51d
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-12-20 00:05:22 +0900

    [book2] Section 05-02

--- app/models/allowed_source.rb
+++ app/models/allowed_source.rb
@@ -1,9 +1,33 @@
 class AllowedSource < ApplicationRecord
+  attr_accessor :last_octet, :_destroy
+
+  before_validation do
+    if last_octet
+      self.last_octet.strip!
+      if last_octet == "*"
+        self.octet4 = 0
+        self.wildcard = true
+      else
+        self.octet4 = last_octet
+      end
+    end
+  end
+
   validates :octet1, :octet2, :octet3, :octet4, presence: true,
     numericality: { only_integer: true, allow_blank: true },
     inclusion: { in: 0..255, allow_blank: true }
   validates :octet4,
     uniqueness: { scope: [ :octet1, :octet2, :octet3 ], allow_blank: true }
+  validates :last_octet,
+    inclusion: { in: (0..255).to_a.map(&:to_s) + [ "*" ], allow_blank: true }
+
+  after_validation do
+    if last_octet
+      errors[:octet4].each do |message|
+        errors.add(:last_octet, message)
+      end
+    end
+  end
 
   def ip_address=(ip_address)
     octets = ip_address.split(".")
