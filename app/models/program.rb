3e9de7cb09776033b21742610d8a966e4a10e4e9
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-12-27 10:39:15 +0900

    [book2] fixes

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,5 +1,4 @@
 class Program < ApplicationRecord
-  has_many :entries, dependent: :destroy
   has_many :entries, dependent: :restrict_with_exception
   has_many :applicants, through: :entries, source: :customer
   belongs_to :registrant, class_name: "StaffMember"
