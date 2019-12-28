a2512f5f9d569d0a867d43d9888b2795af8326be
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-12-28 18:40:13 +0900

    [book2] Section 08-01

--- config/routes.rb
+++ config/routes.rb
@@ -37,6 +37,7 @@ Rails.application.routes.draw do
       root "top#index"
       get "login" => "sessions#new", as: :login
       resource :session, only: [ :create, :destroy ]
+      resources :programs, only: [ :index, :show ]
     end
   end
 end
