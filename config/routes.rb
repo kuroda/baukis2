c6538202ae99454e9ebf5125248611fc1505e5bc
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-12-31 16:20:26 +0900

    [book2/ch12] Section 12-5 A.3

--- config/routes.rb
+++ config/routes.rb
@@ -63,7 +63,7 @@ Rails.application.routes.draw do
           patch :cancel
         end
       end
-      resources :messages, only: [ :index, :show, :new, :create ] do
+      resources :messages, except: [ :edit, :update ] do
         post :confirm, on: :collection
       end
     end
