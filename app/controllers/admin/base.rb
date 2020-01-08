0071b8c112cd5c59849e6b2581c70401dfe75432
Keiko Fujiyama <f.partner.rev2009@gmail.com>
2020-01-08 18:44:46 +0900

    [book2/ch05] Section 3

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,4 +1,5 @@
 class Admin::Base < ApplicationController
+  before_action :check_source_ip_address
   before_action :authorize
   before_action :check_account
   before_action :check_timeout
@@ -12,6 +13,10 @@ class Admin::Base < ApplicationController
 
   helper_method :current_administrator
 
+  private def check_source_ip_address
+    raise IpAddressRejected unless AllowedSource.include?("admin", request.ip)
+  end
+  
   private def authorize
     unless current_administrator
       flash.alert = "管理者としてログインしてください。"
--- config/initializers/baukis2.rb
+++ config/initializers/baukis2.rb
@@ -3,6 +3,6 @@ Rails.application.configure do
     staff: { host: "baukis2.example.com", path: "" },
     admin: { host: "baukis2.example.com", path: "admin" },
     customer: { host: "example.com", path: "mypage" },
-    restrict_ip_addresses: true
+    restrict_ip_addresses: ENV["RESTRICT_IP_ADDRESS"] == "1"
   }
 end
