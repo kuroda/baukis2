a86492cca327f1867e5ddf6ca5793359a7d2ddd4
Keiko Fujiyama <f.partner.rev2009@gmail.com>
2020-01-13 09:29:08 +0900

    [book2/ch07] Section 2

--- app/models/program.rb
+++ app/models/program.rb
@@ -31,4 +31,45 @@ class Program < ApplicationRecord
       self.application_end_minute = application_end_time.min
     end
   end
+
+  before_validation :set_application_start_time
+  before_validation :set_application_end_time
+
+  private def set_application_start_time
+    if t = application_start_date&.in_time_zone
+      self.application_start_time = t.advance(
+        hours: application_start_hour,
+        minutes: application_start_minute
+      )
+    end
+  end
+
+  private def set_application_end_time
+    if t = application_end_date&.in_time_zone
+      self.application_end_time = t.advance(
+        hours: application_end_hour,
+        minutes: application_end_minute
+      )
+    end
+  end
+
+  validates :title, presence: true, length: { maximum: 32 }
+  validates :description, presence: true, length: { maximum: 800 }
+  validates :application_start_time, date: {
+    after_or_equal_to: Time.zone.local(2000, 1, 1),
+    before: -> (obj) { 1.year.from_now },
+    allow_blank: true
+  }
+  validates :application_end_time, date: {
+    after: :application_start_time,
+    before: -> (obj) { obj.application_start_time.advance(days: 90) },
+    allow_blank: true,
+    if: -> (obj) { obj.application_start_time }
+  }
+  validate do
+    if min_number_of_participants && max_number_of_participants &&
+        min_number_of_participants > max_number_of_participants
+      errors.add(:max_number_of_participants, :less_than_min_number)
+    end
+  end 
 end
--- app/presenters/program_form_presenter.rb
+++ app/presenters/program_form_presenter.rb
@@ -4,18 +4,26 @@ class ProgramFormPresenter < FormPresenter
       m << decorated_label(:description, "詳細", required: true)
       m << text_area(:description, rows: 6, style: "width: 454px")
       m.span "（800⽂字以内）", class: "instruction", style: "float: right"
+      m << error_messages_for(:description)
     end
   end
 
   def datetime_field_block(name, label_text, options = {})
     instruction = options.delete(:instruction)
-    markup(:div, class: "input-block") do |m|
+    if object.errors.include?("#{name}_time".to_sym)
+      html_class = "input-block with-errors"
+    else
+      html_class = "input-block"
+    end
+    markup(:div, class: html_class) do |m|
       m << decorated_label("#{name}_date", label_text, options)
       m << date_field("#{name}_date", options)
       m << form_builder.select("#{name}_hour", hour_options)
       m << ":"
       m << form_builder.select("#{name}_minute", minute_options)
       m.span "（#{instruction}）", class: "instruction" if instruction
+      m << error_messages_for("#{name}_time".to_sym)
+      m << error_messages_for("#{name}_date".to_sym)
     end
   end
 
