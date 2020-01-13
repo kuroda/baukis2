521e5821ce43e6e768c565e19e1c2ee1b9a1b720
Keiko Fujiyama <f.partner.rev2009@gmail.com>
2020-01-13 17:51:15 +0900

    [book2/ch08] Section 4

--- app/controllers/customer/entries_controller.rb
+++ app/controllers/customer/entries_controller.rb
@@ -6,6 +6,21 @@ class Customer::EntriesController < Customer::Base
       flash.notice = "プログラムに申し込みました。"
     when :full
       flash.alert = "プログラムへの申込者数が上限に達しました。"
+    when :closed
+      flash.alert = "プログラムの申し込み期間が終了しました。"
+    end
+    redirect_to [ :customer, program ]
+  end
+
+  # PATCH
+  def cancel
+    program = Program.published.find(params[:program_id])
+    if program.application_end_time.try(:<, Time.current)
+      flash.alert = "プログラムへの申し込みをキャンセルできません（受付期間終了）。"
+    else
+      entry = program.entries.find_by!(customer_id: current_customer.id)
+      entry.update_column(:canceled, true)
+      flash.notice = "プログラムへの申し込みをキャンセルしました。"
     end
     redirect_to [ :customer, program ]
   end
--- app/services/customer/entry_acceptor.rb
+++ app/services/customer/entry_acceptor.rb
@@ -4,9 +4,13 @@ class Customer::EntryAcceptor
   end
 
   def accept(program)
+    raise if Time.current < program.application_start_time
+    return :closed if Time.current >= program.application_end_time
     ActiveRecord::Base.transaction do
       program.lock!
-      if max = program.max_number_of_participants
+      if program.entries.where(customer_id: @customer.id).exists?
+        return :accepted
+      elsif max = program.max_number_of_participants
         if program.entries.where(canceled: false).count < max
           program.entries.create!(customer: @customer)
           return :accepted
