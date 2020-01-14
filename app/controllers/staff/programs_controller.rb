5777fbf5b03f3ec25e23ac0d850ef1e652490601
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2020-01-14 20:10:51 +0900

    [book2] Section 6-3

--- app/controllers/staff/programs_controller.rb
+++ app/controllers/staff/programs_controller.rb
@@ -1,10 +1,9 @@
 class Staff::ProgramsController < Staff::Base
   def index
-    @programs = Program.order(application_start_time: :desc)
-      .page(params[:page])
+    @programs = Program.listing.page(params[:page])
   end
 
   def show
-    @program = Program.find(params[:id])
+    @program = Program.listing.params[:id]
   end
 end
