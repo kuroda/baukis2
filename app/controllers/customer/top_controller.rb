d88ba54c49b14087d33c7673b1155fb7b5711db2
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2020-01-15 16:08:44 +0900

    [book2] Section 8-1

--- app/controllers/customer/top_controller.rb
+++ app/controllers/customer/top_controller.rb
@@ -2,6 +2,10 @@ class Customer::TopController < Customer::Base
   skip_before_action :authorize
 
   def index
-    render action: "index"
+    if current_customer
+      render action: "dashboard"
+    else
+      render action: "index"
+    end
   end
 end
