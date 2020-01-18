ed9923b3665f8cb5b9a75793d3e12b41a0c44eff
Keiko Fujiyama <f.partner.rev2009@gmail.com>
2020-01-18 21:30:16 +0900

    [book2/ch09] Section 1

---
+++ app/controllers/customer/accounts_controller.rb
@@ -0,0 +1,21 @@
+class Customer::AccountsController < Customer::Base
+  def show
+    @customer = current_customer
+  end
+
+  def edit
+    @customer_form = Customer::AccountForm.new(current_customer)
+  end
+
+  def update
+    @customer_form = Customer::AccountForm.new(current_customer)
+    @customer_form.assign_attributes(params[:form])
+    if @customer_form.save
+      flash.notice = "アカウント情報を更新しました。"
+      redirect_to :customer_account
+    else
+      flash.now.alert = "⼊⼒に誤りがあります。"
+      render action: "edit"
+    end
+  end
+end
