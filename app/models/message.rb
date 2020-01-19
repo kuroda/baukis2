e6044bbcbc4c43abdee633a491fb447aa2bc999f
Keiko Fujiyama <f.partner.rev2009@gmail.com>
2020-01-19 13:17:38 +0900

    [book2/ch10] Section 1

---
+++ app/models/message.rb
@@ -0,0 +1,19 @@
+class Message < ApplicationRecord
+  belongs_to :customer
+  belongs_to :staff_member, optional: true
+  belongs_to :root, class_name: "Message", foreign_key: "root_id",
+    optional: true
+  belongs_to :parent, class_name: "Message", foreign_key: "parent_id",
+    optional: true
+
+  validates :subject, :body, presence: true
+  validates :subject, length: { maximum: 80, allow_blank: true }
+  validates :body, length: { maximum: 800, allow_blank: true }
+
+  before_validation do
+    if parent
+      self.customer = parent.customer
+      self.root = parent.root || parent
+    end
+  end
+end
