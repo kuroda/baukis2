8562f671e8f0482bc626eadbb53e49231eb1d600
Keiko Fujiyama <f.partner.rev2009@gmail.com>
2020-01-26 09:26:35 +0900

    [book2/ch11] Section 2

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,7 +5,9 @@ class Message < ApplicationRecord
     optional: true
   belongs_to :parent, class_name: "Message", foreign_key: "parent_id",
     optional: true
-
+  has_many :children, class_name: "Message", foreign_key: "parent_id",
+    dependent: :destroy
+    
   validates :subject, :body, presence: true
   validates :subject, length: { maximum: 80, allow_blank: true }
   validates :body, length: { maximum: 800, allow_blank: true }
