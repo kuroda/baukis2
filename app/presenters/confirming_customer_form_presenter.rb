58282d72b52f57ee4e5ac9a50f80f29ac8c9d286
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2020-01-28 15:52:58 +0900

    [book2] Section 9-3

---
+++ app/presenters/confirming_customer_form_presenter.rb
@@ -0,0 +1,9 @@
+class ConfirmingCustomerFormPresenter < ConfirmingUserFormPresenter
+  def gender_field_block
+    markup(:div, class: "input-block") do |m|
+      m << decorated_label(:gender, "性別")
+      m.div(object.gender == "male" ? "男性" : "女性", class: "field-value")
+      m << hidden_field(:gender)
+    end
+  end
+end
---
+++ app/presenters/confirming_form_presenter.rb
@@ -0,0 +1,47 @@
+class ConfirmingFormPresenter
+  include HtmlBuilder
+
+  attr_reader :form_builder, :view_context
+  delegate :label, :hidden_field, :object, to: :form_builder
+
+  def initialize(form_builder, view_context)
+    @form_builder = form_builder
+    @view_context = view_context
+  end
+
+  def notes
+    ""
+  end
+
+  def text_field_block(name, label_text, options = {})
+    markup(:div) do |m|
+      m << decorated_label(name, label_text)
+      if options[:disabled]
+        m.div(object.send(name), class: "field-value readonly")
+      else
+        m.div(object.send(name), class: "field-value")
+        m << hidden_field(name, options)
+      end
+    end
+  end
+
+  def date_field_block(name, label_text, options = {})
+    markup(:div) do |m|
+      m << decorated_label(name, label_text)
+      m.div(object.send(name), class: "field-value")
+      m << hidden_field(name, options)
+    end
+  end
+
+  def drop_down_list_block(name, label_text, choices, options = {})
+    markup(:div) do |m|
+      m << decorated_label(name, label_text)
+      m.div(object.send(name), class: "field-value")
+      m << hidden_field(name, options)
+    end
+  end
+
+  def decorated_label(name, label_text)
+    label(name, label_text)
+  end
+end
